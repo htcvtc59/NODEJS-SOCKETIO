04a94c4b2af6b9ccd1a997d0113986413a596de0
htcvtc59 <htcvtc59@gmail.com>
2017-07-07 14:08:08 +0700

    Lab1

--- index.js
+++ index.js
@@ -9,21 +9,13 @@ var io = require("socket.io")(server);
 server.listen(3000);
 
 io.on("connection", function (socket) {
-    console.log("code socketio" + socket.id);
-    socket.on("disconnect", function () {
-        console.log("code ngat ket noi" + socket.id);
-    });
-    var arr = [];
-    arr.push(socket.id);
-    socket.on("Client-Send-Data", function (data) {
-        console.log(data);
-        io.sockets.emit("Client-Send-Data", data+"123<br/>"); //Phat tat ca 
-        //socket.emit("Client-Send-Data", data+"123<br/>"); //Phat cho chinh no
-        //socket.broadcast.emit("Client-Send-Data", data + "123<br/>"); //Chi phat cho con khong phat cho chinh no
+    console.log("Start " + socket.id);
 
-        //io.to(arr[1]).emit("Client-Send-Data", data + "123<br/>");
+    socket.on("Client-Send-Color", function (data) {
+        io.sockets.emit("ServerSendC",data);
 
     });
+
 });
 
 app.get("/", function (req, res) {
